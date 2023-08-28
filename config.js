7266df5143add12f9f245833d82ef8c8a4b7e8f0
akasur00 <it.willihuebner@protonmail.com>
2023-08-28 10:48:23 +0200

    more changes

--- config.js
+++ config.js
@@ -36,10 +36,10 @@ const CONFIG = {
 	defaultLongitude: '17.834',
 
 	// Autochange
-	autoChangeTheme: true,
+	autoChangeTheme: false,
 
 	// Autochange by OS
-	changeThemeByOS: true,
+	changeThemeByOS: false,
 
 	// Autochange by hour options (24hrs format, string must be in: hh:mm)
 	changeThemeByHour: true,
